cec14a8736e92c7f7184182be3830dda4e36cb24
shashankS89 <shashanksagar212@gmail.com>
2023-03-09 13:35:04 +0530

    completed all apis

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 from database import db, ma
 from modal import Product, Review
 from datetime import datetime
-from sqlalchemy.types import Unicode 
 import os
 
 # Init app
@@ -38,7 +37,7 @@ review_schema = ReviewSchema()
 reviews_schema = ReviewSchema(many=True)
 
 # Create a Product
-@app.route('/product', methods=['POST'])
+@app.route('/list', methods=['POST'])
 def add_product():
   status = request.json['status']
   copy_right = request.json['copy_right']
@@ -55,13 +54,50 @@ def add_product():
 
   return product_schema.jsonify(new_product)
 
-# Get All Products
-@app.route('/products', methods=['GET'])
+@app.route('/lists', methods=['GET'])
 def get_products():
   all_products = Product.query.all()
   result = products_schema.dump(all_products)
   return jsonify(result)
 
+# Get All Best-Sellers NYT
+@app.route('/lists/names.json', methods=['GET'])
+def get_bestseller_NYT():
+  all_products = Product.query.all()
+  result = products_schema.dump(all_products)
+  book=[]
+  for lists in result:
+    for list in lists["results"]["lists"]:
+        book.append(list["books"][0])
+  return jsonify(book)
+
+# Get All Best-Sellers NYT based on date and name
+@app.route('/lists/<date>/<name>', methods=['GET'])
+def get_books_with_date_and_name(date,name):
+  all_products = Product.query.all()
+  result = products_schema.dump(all_products)
+  books=[]
+  for element in result:
+    if element["results"]["bestsellers_date"]==date:
+        for list in element["results"]["lists"]:
+            if list["list_name_encoded"]==name[:-5]:
+                books.append(list)       
+  return jsonify(books)
+
+# Get All Best-Sellers NYT based on current date and name
+@app.route('/lists/current/<name>', methods=['GET'])
+def get_books_with_current_date_and_name(name):
+  all_products = Product.query.all()
+  result = products_schema.dump(all_products)
+  books=[]
+  sorted_result_list_by_bestseller_date=sorted(result, key=lambda x:x["results"]["bestsellers_date"])
+  current_date=sorted_result_list_by_bestseller_date[0]["results"]["bestsellers_date"]
+  for element in result:
+    if element["results"]["bestsellers_date"]==current_date:
+        for list in element["results"]["lists"]:
+            if list["list_name_encoded"]==name[:-5]:
+                books.append(list)       
+  return jsonify(books)
 
 # Create a Review
 @app.route('/review', methods=['POST'])
@@ -87,6 +123,8 @@ def get_reviews():
   result = reviews_schema.dump(all_reviews)
   return jsonify(result)
 
+
+# Get reviews based on params
 @app.route('/reviews.json', methods=['GET'])
 def get_review_query():
     book_author=request.args.get("author")
@@ -99,7 +137,7 @@ def get_review_query():
     elif isbn13:
         all_reviews = Review.query.filter(Review.results[0]["isbn13"].contains([isbn13]))
         
-    result = reviews_schema.dump(all_reviews) 
+    result = reviews_schema.dump(all_reviews)
     return jsonify(result)
 
 
